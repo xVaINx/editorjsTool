10db49fbc97e884ee01957f5d719b80bd89ffcc1
xVaINx <ayushsp19@gmail.com>
2023-12-25 21:34:56 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,114 @@
+import EditorJS from '@editorjs/editorjs';
+/**
+ * Block Tools for the Editor
+ */
+import Header from '@editorjs/header';
+import Image from '@editorjs/image';
+import CodeTool from '@editorjs/code';
+import List from '@editorjs/list';
+import Delimiter from '@editorjs/delimiter';
+import Table from '@editorjs/table';
+import Warning from '@editorjs/warning';
+import Checklist from '@editorjs/checklist';
+import LinkTool from '@editorjs/link';
+import RawTool from '@editorjs/raw';
+import Embed from '@editorjs/embed'; 
+
+/** 
+ * Inline Tools for the Editor
+ */
+import InlineCode from '@editorjs/inline-code';
+import Marker from '@editorjs/marker';
+
+  const defaultConfig = {
+    tools: {
+      header: {
+        class: Header,
+        inlineToolbar: ['marker', 'inlineCode'],
+        config: {
+          placeholder: options.headerPlaceholder || '',
+        },
+      },
+
+      image: {
+        class: Image,
+        inlineToolbar: true,
+        config: {
+          types: 'image/*, video/mp4',
+          endpoints: {
+            byFile: '/api/transport/image',
+            byUrl: '/api/transport/fetch',
+          },
+        },
+      },
+
+      linkTool: {
+        class: LinkTool,
+        config: {
+          endpoint: '/api/fetchUrl',
+        },
+      },
+
+      code: {
+        class: CodeTool,
+        shortcut: 'CMD+SHIFT+D',
+      },
+
+      list: {
+        class: List,
+        inlineToolbar: true,
+      },
+
+      delimiter: Delimiter,
+
+      table: {
+        class: Table,
+        inlineToolbar: true,
+      },
+
+      warning: {
+        class: Warning,
+        inlineToolbar: true,
+      },
+
+      checklist: {
+        class: Checklist,
+        inlineToolbar: true,
+      },
+
+      /**
+       * Inline Tools
+       */
+      inlineCode: {
+        class: InlineCode,
+        shortcut: 'CMD+SHIFT+C',
+      },
+
+      marker: {
+        class: Marker,
+        shortcut: 'CMD+SHIFT+M',
+      },
+
+      raw: RawTool,
+
+      embed: Embed,
+    },
+    data: {
+      blocks: [
+        {
+          type: 'header',
+          data: {
+            text: '',
+            level: 2,
+          },
+        },
+      ],
+    },
+    /**
+    *  onReady callback
+    */
+    onReady: () => {console.log('Editor.js is ready to work!')},
+  };
+
+
+const editor = new EditorJS(defaultConfig);
---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html'
+        })
+    ],
+    devServer: {
+        contentBase: './dist',
+        open: true
+    },
+    mode: 'development'
+};
